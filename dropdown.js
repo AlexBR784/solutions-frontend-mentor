e85cb4e3a159b85f1ff7fcb9a95a2c16756367d6
AlexBR784 <alexbr784@gmail.com>
2022-11-11 19:50:02 +0100

    badges

--- dropdown.js
+++ dropdown.js
@@ -30,9 +30,12 @@ const projects = [
 ];
 
 const ul = document.getElementById("dropUl");
+const dropDiv = document.getElementById("drop");
 const search = document.querySelector(".form-control");
 
 const projectsLen = projects.length;
+dropDiv.innerHTML += `<p class='text-center text-muted mb-0'>Currently there are <span class="badge text-bg-secondary">${projectsLen}</span> solutions completed!</p>`;
+
 console.log(search);
 const onSubmit = (e) => {
   e.preventDefault();
